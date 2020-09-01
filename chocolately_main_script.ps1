de54f55e9e144fd7d6b9c0027f1a189a27f90597
paul novikov <novikov.p@adv.ru>
2020-09-01 16:15:27 +0300

    syntax fix

--- chocolately_main_script.ps1
+++ chocolately_main_script.ps1
@@ -1,8 +1,8 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 $Packages = 'googlechrome', 'notepadplusplus', 'zoom', 'whatsapp' , 'telegram', 'vlc','lightshot','adobereader','7zip','snagit'
 
 ForEach ($PackageName in $Packages)
 {
     choco install $PackageName -y
-})
+}
