d2a9fd777d115cad1240d8ef18984b519e2741cc
pavelmetalarm <49231300+pavelmetalarm@users.noreply.github.com>
2020-09-01 16:40:12 +0300

    Update chocolately_main_script.ps1
    firefox

--- chocolately_main_script.ps1
+++ chocolately_main_script.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-$Packages = 'googlechrome', 'notepadplusplus','openvpn','git', 'zoom', 'whatsapp' ,'putty', 'winscp','bitrix24','telegram', 'vlc','lightshot','adobereader','7zip','snagit'
+$Packages = 'googlechrome', 'notepadplusplus','openvpn','git', 'zoom', 'whatsapp' ,'putty', 'winscp','bitrix24','telegram', 'vlc','lightshot','firefox','adobereader','7zip','snagit'
 
 ForEach ($PackageName in $Packages)
 {
