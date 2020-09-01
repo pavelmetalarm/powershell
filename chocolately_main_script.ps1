ddc8f8ad07d7b74059bf9be3152c6f88d82176c1
paul novikov <novikov.p@adv.ru>
2020-09-01 16:26:08 +0300

    added openvpn

--- chocolately_main_script.ps1
+++ chocolately_main_script.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-$Packages = 'googlechrome', 'notepadplusplus', 'zoom', 'whatsapp' , 'telegram', 'vlc','lightshot','adobereader','7zip','snagit'
+$Packages = 'googlechrome', 'notepadplusplus','openvpn', 'zoom', 'whatsapp' , 'telegram', 'vlc','lightshot','adobereader','7zip','snagit'
 
 ForEach ($PackageName in $Packages)
 {
