93ead5b81ab6518004bb3cc55689cb1d3f0114b6
pavelmetalarm <49231300+pavelmetalarm@users.noreply.github.com>
2020-09-02 11:32:59 +0300

    Update chocolately_main_script.ps1
    deleted chrome,bitrix24, 7zip

--- chocolately_main_script.ps1
+++ chocolately_main_script.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-$Packages = 'googlechrome', 'notepadplusplus','openvpn','git', 'zoom', 'whatsapp' ,'putty', 'winscp','bitrix24','telegram', 'vlc','lightshot','firefox','adobereader','7zip'
+$Packages =  'notepadplusplus','openvpn','git', 'zoom', 'whatsapp' ,'putty', 'winscp','telegram', 'vlc','lightshot','firefox','adobereader'
 
 ForEach ($PackageName in $Packages)
 {
