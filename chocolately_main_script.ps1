6b93f885931632d0f6e42531bf24e65ee83943fb
pavelmetalarm <49231300+pavelmetalarm@users.noreply.github.com>
2020-09-09 16:54:51 +0300

    Update chocolately_main_script.ps1
    added flashbackexpress

--- chocolately_main_script.ps1
+++ chocolately_main_script.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-$Packages =  'notepadplusplus','openvpn','git', 'zoom', 'whatsapp' ,'putty', 'winscp','telegram', 'vlc','lightshot','firefox','adobereader'
+$Packages =  'notepadplusplus','openvpn','git', 'zoom', 'whatsapp' ,'putty', 'winscp','telegram', 'vlc','lightshot','firefox','adobereader','flashbackexpress'
 
 ForEach ($PackageName in $Packages)
 {
