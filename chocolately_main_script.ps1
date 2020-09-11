7ab9f84487ea4227070d77ada2d84c11a1629bc8
pavelmetalarm <49231300+pavelmetalarm@users.noreply.github.com>
2020-09-11 13:55:33 +0300

    Update chocolately_main_script.ps1
    added goggle chrome

--- chocolately_main_script.ps1
+++ chocolately_main_script.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-$Packages =  'notepadplusplus','openvpn','git', 'zoom', 'whatsapp' ,'putty', 'winscp','telegram', 'vlc','lightshot','firefox','adobereader','flashbackexpress'
+$Packages =  'notepadplusplus','openvpn','git', 'zoom', 'whatsapp' ,'putty', 'winscp','telegram', 'vlc','lightshot','firefox','adobereader','flashbackexpress', 'googlechrome'
 
 ForEach ($PackageName in $Packages)
 {
