1515c0d95ca03e32c7fe3b79fcd0ff6f1390e5df
pavelmetalarm <49231300+pavelmetalarm@users.noreply.github.com>
2020-09-15 12:24:24 +0300

    Update chocolately_main_script.ps1
    changes office distr

--- chocolately_main_script.ps1
+++ chocolately_main_script.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-$Packages =  'notepadplusplus','openvpn','git', 'zoom', 'whatsapp' ,'putty', 'winscp','telegram', 'vlc','lightshot','firefox','adobereader','flashbackexpress', 'googlechrome', 'office365business','7Zip','puntoswitcher','Zeplin'
+$Packages =  'notepadplusplus','openvpn','git', 'zoom', 'whatsapp' ,'putty', 'winscp','telegram', 'vlc','lightshot','firefox','adobereader','flashbackexpress', 'googlechrome', 'office365proplus','7Zip','puntoswitcher','Zeplin'
 
 ForEach ($PackageName in $Packages)
 {
